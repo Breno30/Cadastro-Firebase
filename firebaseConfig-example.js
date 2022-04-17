036279dd385458213b91cf323af6c475e0ea2e0b
breno30 <brenodonascimentosilvaa@gmail.com>
2022-04-17 20:41:25 -0300

    Examplo de configuração do firebase

---
+++ firebaseConfig-example.js
@@ -0,0 +1,9 @@
+﻿const firebaseConfig = {
+  apiKey: "###########################",
+  authDomain: "#############.firebaseapp.com",
+  databaseURL: "https://########.firebaseio.com",
+  projectId: "########",
+  storageBucket: "##########.appspot.com",
+  messagingSenderId: "9999999999999",
+  appId: "9:999#999#9#999:web:9#99999#9999#"
+};
